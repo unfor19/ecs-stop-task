c0590afaf014b272cf7aedfb79b1176a04b6403c
Meir Gabay <unfor19@gmail.com>
2022-05-28 11:18:24 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,8 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 source bargs.sh "$@"
+set -e
+set -o pipefail
 
-task_arn=$(aws ecs list-tasks --max-items 1 --cluster ${cluster_name} --family ${family_name} | jq -r .taskArns[0])
-if [[ -z $(echo "$task_arn" | grep "An error occurred") ]] ; then
+task_arn=$(aws ecs list-tasks --max-items 1 --cluster ${cluster_name} --family ${family_name} | jq -r .taskArns[0] || true)
+if [[ -n "$(echo "$task_arn" | grep "An error occurred") ]]" ; then
     echo "$task_arn"
     exit 1
 fi
