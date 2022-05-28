57c6ee04ee7519f3fb71bdcc63fbfe0bc0f86764
Meir Gabay <unfor19@gmail.com>
2022-05-28 11:25:22 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,7 +4,7 @@ set -e
 set -o pipefail
 
 task_arn=$(aws ecs list-tasks --max-items 1 --cluster ${cluster_name} --family ${family_name} | jq -r .taskArns[0] || true)
-if [[ -n "$(echo "$task_arn" | grep "An error occurred") ]]" ; then
+if [[ -n "$(echo "$task_arn" | grep "An error occurred")" ]] ; then
     echo "$task_arn"
     exit 1
 fi
