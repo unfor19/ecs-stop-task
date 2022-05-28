118dcc3fd5792f96e230e5c7cfd42a9cf21a74ec
Meir Gabay <unfor19@gmail.com>
2022-05-28 11:30:17 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,7 +5,14 @@ set -o pipefail
 
 task_arn=$(aws ecs list-tasks --max-items 1 --cluster ${cluster_name} --family ${family_name} | jq -r .taskArns[0] || true)
 if [[ -n "$(echo "$task_arn" | grep "An error occurred")" ]] ; then
-    echo "$task_arn"
+    echo "ERR 1 :: $task_arn"
     exit 1
+elif [[ -n "$(echo "$task_arn" | grep "taskId length should be one of")" ]]; then
+    echo "ERR 254 :: $task_arn"
+    echo "No tasks to stop, exit successfully with 0"
+    exit 0
 fi
+
+echo "Stopping task ..."
+export AWS_PAGER=""
 aws ecs stop-task --cluster "${cluster_name}" --task "${task_arn}" --reason ${reason}
